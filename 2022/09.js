6cfebb23c9f0a8bdf94201b2295f3fb9f8562bf4
Nick Rokosz <nicholasrokosz@gmail.com>
2022-12-09 19:16:27 -0700

    Add day 9 final solution

--- 2022/09.js
+++ 2022/09.js
@@ -15,28 +15,44 @@ const diagonalMoves = {
   DL: ({x, y}) => ({ x: x - 1, y: y - 1 }),
 }
 
-const decideTailMove = (dir) => {
-  if (h.x === t.x || h.y === t.y) return orthogonalMoves[dir]
-  if (h.x > t.x && h.y > t.y) return diagonalMoves['UR']
-  if (h.x > t.x && h.y < t.y) return diagonalMoves['DR']
-  if (h.x < t.x && h.y < t.y) return diagonalMoves['DL']
-  if (h.x < t.x && h.y > t.y) return diagonalMoves['UL']
+const decideKnotMove = (knot, knotAhead) => {
+  if (knotAhead.x === knot.x && knotAhead.y > knot.y) return orthogonalMoves['U']
+  if (knotAhead.x === knot.x && knotAhead.y < knot.y) return orthogonalMoves['D']
+  if (knotAhead.x > knot.x && knotAhead.y === knot.y) return orthogonalMoves['R']
+  if (knotAhead.x < knot.x && knotAhead.y === knot.y) return orthogonalMoves['L']
+  if (knotAhead.x > knot.x && knotAhead.y > knot.y) return diagonalMoves['UR']
+  if (knotAhead.x > knot.x && knotAhead.y < knot.y) return diagonalMoves['DR']
+  if (knotAhead.x < knot.x && knotAhead.y < knot.y) return diagonalMoves['DL']
+  if (knotAhead.x < knot.x && knotAhead.y > knot.y) return diagonalMoves['UL']
 }
 
 const areTouching = (pt1, pt2) => (pt1.x >= pt2.x - 1 && pt1.x <= pt2.x + 1) && (pt1.y >= pt2.y - 1 && pt1.y <= pt2.y + 1)
 
-let h = { x: 0, y: 0 }, t = { x: 0, y: 0 }, pointsVisited = { '0,0': true }
-
-for (const move of input.split('\n').slice(0, -1)) {
-  const [dir, len] = move.split(' ')
-
-  for (const _ of Array(+len)) {
-    h = orthogonalMoves[dir](h)
-    if (areTouching(h, t)) continue
-    t = decideTailMove(dir)(t)
-    const ptStr = `${t.x},${t.y}`
-    if (!pointsVisited[ptStr]) pointsVisited[ptStr] = true
-  }
+const moveKnot = (knot, knotAhead, dir) => {
+  if (!knotAhead) return orthogonalMoves[dir](knot)
+  if (areTouching(knot, knotAhead)) return knot
+  return decideKnotMove(knot, knotAhead)(knot)
 }
 
-console.log(Object.keys(pointsVisited).length)
+const main = (input, numberOfKnots) => {
+  const positions = [...Array(numberOfKnots)].map(_ => ({ x: 0, y: 0 }))
+  const pointsVisited = { '0,0': true }
+
+  for (const move of input.split('\n').slice(0, -1)) {
+    const [dir, len] = move.split(' ')
+
+    for (const _ of Array(+len)) {
+      for (const [i, knot] of positions.entries()) {
+        positions[i] = moveKnot(knot, positions[i - 1], dir)
+
+        if (i === numberOfKnots - 1) {
+          const ptStr = `${positions[i].x},${positions[i].y}`
+          if (!pointsVisited[ptStr]) pointsVisited[ptStr] = true
+        }
+      }
+    }
+  }
+  return Object.keys(pointsVisited).length
+}
+
+console.log(`Answer 1: ${main(input, 2)}, Answer 2: ${main(input, 10)}`)
