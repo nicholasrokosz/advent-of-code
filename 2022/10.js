f1d5b65ba37406bb9f93151b681962d2d4e1f042
Nick Rokosz <nicholasrokosz@gmail.com>
2022-12-10 22:28:38 -0700

    Add day 10 final solution

--- 2022/10.js
+++ 2022/10.js
@@ -13,8 +13,29 @@ const executeInstruction = instruction => {
 
 const signalStrength = cycleNumber => cycles[cycleNumber - 1] * cycleNumber
 
+// part 1
 for (const instruction of input.split('\n').slice(0, -1)) {
   executeInstruction(instruction)
 }
 
 console.log(signalStrength(20) + signalStrength(60) + signalStrength(100) + signalStrength(140) + signalStrength(180) + signalStrength(220))
+
+// part 2
+let output = ''
+let lineNumber = 0
+
+for (const [index, X] of cycles.entries()) {
+  if (lineNumber === 6) break
+  const sprite = [X - 1, X, X + 1]
+  const cycleNumber = index - (40 * lineNumber)
+
+  if (sprite.includes(cycleNumber)) output += '#'
+  else output += '.'
+
+  if ((index + 1) % 40 === 0) {
+    output += '\n'
+    lineNumber++
+  }
+}
+
+console.log(output)
