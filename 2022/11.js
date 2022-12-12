c7075a12f2841292eb1d9849c486428ad29c91fd
Nick Rokosz <nicholasrokosz@gmail.com>
2022-12-11 18:29:54 -0700

    Add day 11 final solution

--- 2022/11.js
+++ 2022/11.js
@@ -7,20 +7,23 @@ const buildMonkey = desc => {
 
   monkey.items = lines[0].match(/\d+/g).map(str => +str)
   monkey.op = eval(`old => ${lines[1].match(/old.+/g)}`)
-  monkey.test = worry => worry % lines[2].match(/\d+/g)[0] === 0 ? lines[3].slice(-1) : lines[4].slice(-1)
+  monkey.divisibleBy = lines[2].match(/\d+/g)[0]
+  monkey.test = worry => worry % monkey.divisibleBy === 0 ? lines[3].slice(-1) : lines[4].slice(-1)
   monkey.itemsInspected = 0
 
   return monkey
 }
 
 const monkeys = input.split('\n\n').map(desc => buildMonkey(desc))
-// console.log(monkeys[0].test(2))
+const divisor = monkeys.map(monkey => monkey.divisibleBy).reduce((a, b) => a * b)
 
-const takeTurn = monkeyNumber => {
+const takeTurn = (monkeyNumber, part) => {
   const { items, op, test } = monkeys[monkeyNumber]
 
   for (const item of items) {
-    const newValue = Math.floor(op(item) / 3)
+    let newValue
+    if (part === 'part1') newValue = Math.floor(op(item) / 3)
+    else newValue = op(item) % divisor
     const nextMonkey = test(newValue)
 
     monkeys[nextMonkey].items.push(newValue)
@@ -30,18 +33,18 @@ const takeTurn = monkeyNumber => {
   monkeys[monkeyNumber].items = []
 }
 
-const executeRound = () => {
+const executeRound = part => {
   for (const monkey in monkeys) {
-    takeTurn(monkey)
+    takeTurn(monkey, part)
   }
 }
 
-const part1 = numberOfRounds => {
-  for (const _ of Array(numberOfRounds)) {
-    executeRound()
+const main = part => {
+  for (const _ of Array(part === 'part1' ? 20 : 10000)) {
+    executeRound(part)
   }
 
   return monkeys.map(monkey => monkey.itemsInspected).sort((a, b) => a - b).slice(-2).reduce((a, b) => a * b)
 }
 
-console.log(`Answer 1: ${part1(20)}`)
+console.log(main('part2'))
